ff37be5893c35d67697f352128c7dc94eb2a4f29
YvannLogi <yvanndavid03@gmail.com>
2026-01-12 18:35:06 +0000

    android & ios capacitor config added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.task',
+  appName: 'taskas',
+  webDir: 'dist'
+};
+
+export default config;
